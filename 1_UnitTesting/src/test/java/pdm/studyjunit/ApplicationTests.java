1c822b2cee4a81e0c36faaa3b540ca1d6e3ef78f
Ferrorist <ferrorist@naver.com>
2024-08-31 23:09:18 +0900

    test: lifecycle method 추가

--- 1_UnitTesting/src/test/java/pdm/studyjunit/ApplicationTests.java
+++ 1_UnitTesting/src/test/java/pdm/studyjunit/ApplicationTests.java
@@ -5,6 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,18 +17,27 @@ import pdm.studyjunit.Utils.DemoUtils;
 @SpringBootTest
 class ApplicationTests {
 
+	DemoUtils demoUtils;
+
+	@BeforeAll
+	static void beforeAll() {
+		System.out.println("@BeforeAll print!");
+	}
+
+	@BeforeEach
+	void setUp() {
+		demoUtils = new DemoUtils();
+		System.out.println("@BeforeEach print!");
+	}
+
 	@Test
 	void testEqualsAndNotEquals() {
-		DemoUtils demoUtils = new DemoUtils();
-
 		assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
 		assertNotEquals(8, demoUtils.add(2, 4), "2+4 must be not 8");
 	}
 
 	@Test
 	void testNullAndNotNull() {
-		DemoUtils demoUtils = new DemoUtils();
-
 		String str1 = null;
 		String str2 = "luv2code";
 
@@ -32,4 +45,13 @@ class ApplicationTests {
 		assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
 	}
 
+	@AfterEach
+	void tearDown() {
+		System.out.println("@AfterEach print!");
+	}
+
+	@AfterAll
+	static void afterAll() {
+		System.out.println("@AfterAll print!");
+	}
 }
