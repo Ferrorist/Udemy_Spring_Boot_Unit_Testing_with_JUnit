bac8ba9f66b23a1daca86885470a64bea6a88f76
Ferrorist <ferrorist@naver.com>
2024-09-01 23:10:30 +0900

    test: assertSame(NotSame), assertTrue(False) 학습

--- 1_UnitTesting/src/test/java/pdm/studyjunit/ApplicationTests.java
+++ 1_UnitTesting/src/test/java/pdm/studyjunit/ApplicationTests.java
@@ -1,9 +1,6 @@
 package pdm.studyjunit;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -18,7 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import pdm.studyjunit.Utils.DemoUtils;
 
 @SpringBootTest
-@DisplayNameGeneration(DisplayNameGenerator.IndicativeSentences.class)
+// @DisplayNameGeneration(DisplayNameGenerator.IndicativeSentences.class)
 class ApplicationTests {
 
 	DemoUtils demoUtils;
@@ -36,12 +33,38 @@ class ApplicationTests {
 	}
 
 	@Test
+	@DisplayName("Null and Not Null")
 	void testNullAndNotNull() {
 		String str1 = null;
 		String str2 = "luv2code";
 
 		assertNull(demoUtils.checkNull(str1), "Object should be null");
 		assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
+	}
+
+
+	@Test
+	@DisplayName("Same and Not Same")
+	void testSameAndNotSame() {
+		String str = "luv2code";
+
+		assertSame(
+			demoUtils.getAcademy(), demoUtils.getAcademyDuplicate(),
+			"Objects should refer to same object");
+
+		assertNotSame(str, demoUtils.getAcademy(),
+			"Objects should not refer to same object");
+	}
+
+
+	@Test
+	@DisplayName("True and False")
+	void testTrueFalse() {
+		int gradeOne = 10;
+		int gradeTwo = 5;
+
+		assertTrue(demoUtils.isGreater(gradeOne, gradeTwo), "This should return true");
+		assertFalse(demoUtils.isGreater(gradeTwo, gradeOne), "This should return false");
 	}
 
 	// @BeforeAll
