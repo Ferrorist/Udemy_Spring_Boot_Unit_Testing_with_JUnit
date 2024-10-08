333ca6954e112cf457f635e39b8e0ce48e55db24
Ferrorist <ferrorist@naver.com>
2024-10-08 23:10:39 +0900

    init: 2_TDD project

---
+++ 2_TDD/src/main/java/pdm/junitstudytdd/Application.java
@@ -0,0 +1,13 @@
+package pdm.junitstudytdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
