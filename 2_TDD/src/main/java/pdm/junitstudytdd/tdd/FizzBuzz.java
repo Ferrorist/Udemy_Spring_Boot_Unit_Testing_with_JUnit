1e1048590e8926d4f6710d567ec5fae69f074313
Ferrorist <ferrorist@naver.com>
2024-10-08 23:46:50 +0900

    refector: FizzBuzz

--- 2_TDD/src/main/java/pdm/junitstudytdd/tdd/FizzBuzz.java
+++ 2_TDD/src/main/java/pdm/junitstudytdd/tdd/FizzBuzz.java
@@ -7,16 +7,11 @@ public class FizzBuzz {
 	// If number is divisible by 3 and 5, print FizzBuzz
 	// If number is NOT divisible by 3 or 5, then print the number
 	public static String compute(int i) {
-		if(i % (3*5) == 0){
-			return "FizzBuzz";
-		}
-		else if(i % 3 == 0) {
-			return "Fizz";
-		}
-		else if(i % 5 == 0) {
-			return "Buzz";
-		}
-		return Integer.toString(i);
-	}
+		StringBuilder result = new StringBuilder();
+		if(i % 3 == 0)	result.append("Fizz");
+		if(i % 5 == 0)	result.append("Buzz");
+		if(result.isEmpty())	result.append(i);
 
+		return result.toString();
+	}
 }
