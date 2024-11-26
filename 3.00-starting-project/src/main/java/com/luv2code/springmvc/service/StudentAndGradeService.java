19be0ac09f71ea18fe0756a568dbf6bb0fa9fa9e
parkdamin <ferrorist@naver.com>
2024-11-26 23:04:39 +0900

    feat: 내장 데이터베이스 활용한 TDD 실행

--- 3.00-starting-project/src/main/java/com/luv2code/springmvc/service/StudentAndGradeService.java
+++ 3.00-starting-project/src/main/java/com/luv2code/springmvc/service/StudentAndGradeService.java
@@ -2,6 +2,7 @@ package com.luv2code.springmvc.service;
 
 import com.luv2code.springmvc.models.CollegeStudent;
 import com.luv2code.springmvc.repository.StudentDao;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,4 +20,17 @@ public class StudentAndGradeService {
         studentDao.save(student);
     }
 
+    public boolean checkIfStudentIsNull(int id) {
+        Optional<CollegeStudent> student = studentDao.findById(id);
+        if (student.isPresent()) {
+            return true;
+        }
+        return false;
+    }
+
+    public void deleteStudent(int id) {
+        if (checkIfStudentIsNull(id)) {
+            studentDao.deleteById(id);
+        }
+    }
 }
--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -3,17 +3,24 @@ package com.luv2code.springmvc;
 import com.luv2code.springmvc.models.CollegeStudent;
 import com.luv2code.springmvc.repository.StudentDao;
 import com.luv2code.springmvc.service.StudentAndGradeService;
+import java.util.Optional;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 
 // 테스트 시 사용할 properties 설정 가능
 @TestPropertySource("/application.properties")
 @SpringBootTest
 public class StudentAndGradeServiceTest {
+
+    @Autowired
+    private JdbcTemplate jdbc;
 
     @Autowired
     private StudentAndGradeService studentService;
@@ -30,4 +37,31 @@ public class StudentAndGradeServiceTest {
          Assertions.assertEquals("chad.darby@luv2code_school.com", student.getEmailAddress(), "find by email");
     }
 
+    @Test
+    public void isStudentNullCheck() {
+        Assertions.assertTrue(studentService.checkIfStudentIsNull(1));
+        Assertions.assertFalse(studentService.checkIfStudentIsNull(0));
+    }
+
+    @BeforeEach
+    public void setUpDatabase() {
+        jdbc.execute(
+                "insert into student(id, firstname, lastname, email_address) values (1, 'Eric', 'Roby', 'eric.roby@luv2code_school.com')");
+    }
+
+    @Test
+    public void deleteStudentService() {
+        Optional<CollegeStudent> deletedCollegeStudent = studentDao.findById(1);
+        Assertions.assertTrue(deletedCollegeStudent.isPresent(), "Return True");
+
+        studentService.deleteStudent(1);
+
+        deletedCollegeStudent = studentDao.findById(1);
+        Assertions.assertFalse(deletedCollegeStudent.isPresent(), "Return false");
+    }
+
+    @AfterEach
+    public void setupAfterTransaction() {
+        jdbc.execute("delete from student");
+    }
 }
